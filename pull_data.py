d61f778fa29dfb0292ce5cf41a700dab0a42d529
mtrapaglia <matiasdmansilla@hotmail.com>
2025-03-27 02:22:37 -0300

    token global

--- pull_data.py
+++ pull_data.py
@@ -4,7 +4,11 @@ import os
 
 load_dotenv()
 
+bearer = None
+refresh = None
+
 def get_token():
+    global bearer, refresh
     url = "https://api.invertironline.com/token"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
@@ -25,9 +29,32 @@ def get_token():
 
     bearer = response.json()["access_token"]
     refresh = response.json()["refresh_token"]
-    return bearer, refresh
+    return
 
-def call_api(bearer, api_url):
+def update_token():
+    global bearer, refresh
+    url = "https://api.invertironline.com/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "refresh_token": refresh,
+        "grant_type": "refresh_token"
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+
+    if response.status_code == 200:
+        print("Access Token granted")
+    else:
+        print("Error:", response.status_code, response.text)
+        assert(False)
+
+    bearer = response.json()["access_token"]
+    refresh = response.json()["refresh_token"]
+
+def call_api(api_url):
+    global bearer
     headers = {
         "Authorization": f"Bearer {bearer}",
         "Content-Type": "application/json"
@@ -38,16 +65,30 @@ def call_api(bearer, api_url):
     if response.status_code == 200:
         print("API call successful")
     else:
-        print("Error:", response.status_code, response.text)
-        assert(False)
+        print("Renovando token...")
+        update_token()
+        response = requests.get(api_url, headers=headers)
+
+        if response.status_code != 200:
+            print("Error al renovar el token:", response.status_code, response.text)
+            assert(False)
 
     return response.json()
 
-def get_estado_cuenta(bearer):
+def get_estado_cuenta():
     api_url = "https://api.invertironline.com/api/v2/estadocuenta"
-    return call_api(bearer, api_url)
+    rsp = call_api(api_url)
+    saldos_arg = rsp['cuentas'][0]['saldos']
+    print(f"Pesos disponibles: ahora: {saldos_arg[0]['disponible']} en 24hs: {saldos_arg[1]['disponible']} en 48hs: {saldos_arg[2]['disponible']}, comprometido: {saldos_arg[0]['comprometido']}")
+    saldos_dol = rsp['cuentas'][1]['saldos']
+    print(f"Dolares disponibles: ahora: {saldos_dol[0]['disponible']} en 24hs: {saldos_dol[1]['disponible']} en 48hs: {saldos_dol[2]['disponible']}, comprometido: {saldos_dol[0]['comprometido']}")
+    print(f"Titulos valorizados a peso: {rsp['cuentas'][0]['titulosValorizados']}, total: {rsp['cuentas'][0]['total']}")
 
-bearer, refresh = get_token()
-print(get_estado_cuenta(bearer))
+def get_portafolio():
+    api_url = "https://api.invertironline.com/api/v2/portafolio/argentina"
+    rsp = call_api(api_url)
+    print(rsp)
 
-pass
+get_token()
+get_estado_cuenta()
+get_portafolio()
