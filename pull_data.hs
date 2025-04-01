09f8fd3335bc3c3e42983eaa1f11d20ebe83b0e2
mtrapaglia <matiasdmansilla@hotmail.com>
2025-04-01 05:12:23 -0300

    fix

--- pull_data.hs
+++ pull_data.hs
@@ -8,57 +8,6 @@ import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad (mzero)
-import System.IO
-
-loadCreds :: IO (Maybe String)
-loadCreds= do
-    exists <- doesFileExist ".env"
-    if exists then Just <$> readFile "token.txt" else return Nothing
-
--- Tipo de dato para almacenar el token
-data AuthResponse = AuthResponse { accessToken :: String }
-
-instance FromJSON AuthResponse where
-    parseJSON (Object v) = AuthResponse <$> v .: "access_token"
-    parseJSON _ = mzero
-
--- Función para realizar la autenticación y obtener el token
-authenticate :: String -> String -> IO (Maybe String)
-authenticate username password = do
-    manager <- newManager tlsManagerSettings
-    req <- parseRequest "https://api.invertironline.com/token"
-    
-    let body = "username=" ++ username ++ "&password=" ++ password ++ "&grant_type=password"
-    let req' = req
-            { method = "POST"
-            , requestBody = RequestBodyBS (B8.pack body)
-            , requestHeaders = [("Content-Type", "application/x-www-form-urlencoded")]
-            }
-    
-    response <- httpLBS req'
-    let responseBody = getResponseBody response
-    
-    case decode responseBody of
-        Just auth -> return (Just (accessToken auth))
-        Nothing   -> return Nothing
-
--- Función principal para probar la autenticación
-main :: IO ()
-main = do
-    token <- authenticate "MIUSUARIO" "MICONTRASEÑA"
-    case token of
-        Just t  -> putStrLn $ "Token obtenido: " ++ t
-        Nothing -> putStrLn "Error al obtener el token"
-{-# LANGUAGE OverloadedStrings #-}
-
-import Network.HTTP.Simple
-import Network.HTTP.Client (RequestBody (..))
-import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Data.Aeson (decode, Value(..), (.:))
-import qualified Data.ByteString.Char8 as B8
-import qualified Data.ByteString.Lazy.Char8 as L8
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad (mzero)
 import System.Environment (lookupEnv)
 import Configuration.Dotenv (loadFile, defaultConfig)
 
@@ -73,8 +22,8 @@ instance FromJSON AuthResponse where
 getCredentials :: IO (Maybe String, Maybe String)
 getCredentials = do
     loadFile defaultConfig  -- Carga el archivo .env
-    user <- lookupEnv "API_USERNAME"
-    pass <- lookupEnv "API_PASSWORD"
+    user <- lookupEnv "IOL_USERNAME"
+    pass <- lookupEnv "IOL_PASSWORD"
     return (user, pass)
 
 -- Función para realizar la autenticación y obtener el token
